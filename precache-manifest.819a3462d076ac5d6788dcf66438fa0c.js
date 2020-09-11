309260a654e7d3c763e1432f8a82cc4b815807d9
Faiz <faiz@Faizs-iMac.local>
2020-09-11 11:40:28 +0200

    Updates

---
+++ precache-manifest.819a3462d076ac5d6788dcf66438fa0c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0188c858c1dba6ef9dba61c32614b413",
+    "url": "/BNRY-carousel/index.html"
+  },
+  {
+    "revision": "568f05f7fbd5990cc650",
+    "url": "/BNRY-carousel/static/css/2.49d3d760.chunk.css"
+  },
+  {
+    "revision": "e489dea405e87b7eacbe",
+    "url": "/BNRY-carousel/static/css/main.88b8d1a4.chunk.css"
+  },
+  {
+    "revision": "568f05f7fbd5990cc650",
+    "url": "/BNRY-carousel/static/js/2.b27acacc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/BNRY-carousel/static/js/2.b27acacc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e489dea405e87b7eacbe",
+    "url": "/BNRY-carousel/static/js/main.c4533a73.chunk.js"
+  },
+  {
+    "revision": "3c11974c558d907eabe8",
+    "url": "/BNRY-carousel/static/js/runtime-main.fa3d09b0.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/BNRY-carousel/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/BNRY-carousel/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/BNRY-carousel/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/BNRY-carousel/static/media/slick.f97e3bbf.svg"
+  }
+]);
